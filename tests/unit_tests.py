0ff7cb81ebe26df2a220373e2345144b9672ffc8
aureliony <39163684+aureliony@users.noreply.github.com>
2023-07-23 03:33:03 +0800

    Add unit tests for different difficulties

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -3,11 +3,61 @@ sys.path.append('../transcription/')
 import unittest
 import transcribe
 import filecmp
+import os
+
+def removeFile(file):
+    """
+    helper function to remove a file in the current directory, handling OSError if file does not exist.
+    """
+    try:
+        os.remove(file)
+    except OSError:
+        pass
 
 class TestTranscription(unittest.TestCase):
+    """
+    Test transcription of audio file at the default difficulty.
+    """
     def runTest(self):
-        transcribe.transcribe("test.ogg") # generate test.mid
+        removeFile("test.mid")
+        transcribe.transcribe("test.ogg")
         self.assertTrue(filecmp.cmp("test.mid", "test_expected.mid", shallow = False))
+
+class TestDiff0Transcription(unittest.TestCase):
+    """
+    Test transcription of audio file at difficulty 0.
+    """
+    def runTest(self):
+        removeFile("test.mid")
+        transcribe.transcribe("test.ogg",0)
+        self.assertTrue(filecmp.cmp("test.mid", "diff0_expected.mid", shallow = False))
+
+class TestDiff1Transcription(unittest.TestCase):
+    """
+    Test transcription of audio file at difficulty 1.
+    """
+    def runTest(self):
+        removeFile("test.mid")
+        transcribe.transcribe("test.ogg",1)
+        self.assertTrue(filecmp.cmp("test.mid", "diff1_expected.mid", shallow = False))
+
+class TestDiff2Transcription(unittest.TestCase):
+    """
+    Test transcription of audio file at difficulty 2.
+    """
+    def runTest(self):
+        removeFile("test.mid")
+        transcribe.transcribe("test.ogg",2)
+        self.assertTrue(filecmp.cmp("test.mid", "diff2_expected.mid", shallow = False))
+
+class TestDiff3Transcription(unittest.TestCase):
+    """
+    Test transcription of audio file at difficulty 3.
+    """
+    def runTest(self):
+        removeFile("test.mid")
+        transcribe.transcribe("test.ogg",3)
+        self.assertTrue(filecmp.cmp("test.mid", "diff3_expected.mid", shallow = False))
 
 if __name__ == "__main__":
     unittest.main()
--- transcription/transcribe.py
+++ transcription/transcribe.py
@@ -1,4 +1,20 @@
 from omnizart.music.app import MusicTranscription
-model = MusicTranscription()
-def transcribe(file):
+from omnizart.drum.app import DrumTranscription
+# from omnizart.chord.app import ChordTranscription
+from omnizart.vocal.app import VocalTranscription
+from omnizart.beat.app import BeatTranscription
+
+def transcribe(file, difficulty = 3):
+    """
+    Transcribes the audio file at a given difficulty, and outputs the .midi file with the same filename.
+    EASY = 0
+    MEDIUM = 1
+    HARD = 2
+    EXTREME = 3
+    """
+    model = None
+    if difficulty == 0: model = VocalTranscription()
+    elif difficulty == 1: model = DrumTranscription()
+    elif difficulty == 2: model = BeatTranscription()
+    else: model = MusicTranscription()
     model.transcribe(file) # output is file.midi
