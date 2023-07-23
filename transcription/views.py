3c42cdb44668444e64b92aa29576e21aebeee219
aureliony <39163684+aureliony@users.noreply.github.com>
2023-07-24 01:18:00 +0800

    include song_id in POST request

--- transcription/views.py
+++ transcription/views.py
@@ -12,10 +12,11 @@ def transcribe_song(request):
     try:
         # Get the base64 encoded audio data from the request
         base64_data = request.POST.get('base64_data')
+        song_id = request.POST.get('song_id')
         # Perform transcription
         transcription = transcribe_b64(base64_data)
         # Return the transcription as JSON response
-        return JsonResponse({'transcription': transcription})
+        return JsonResponse({'transcription': transcription, 'song_id': song_id})
 
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=400)
