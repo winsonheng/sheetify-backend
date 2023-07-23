e3ed75cfb9c67e189bb0c9cf70dd7397c5b9fc1b
winsonheng <55232476+winsonheng@users.noreply.github.com>
2023-07-24 07:27:06 +0800

    Update procfile

--- orbital_backend/settings.py
+++ orbital_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from google.oauth2 import service_account
 
+import os
 import dj_database_url
 import environ
 env = environ.Env()
@@ -243,6 +244,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
