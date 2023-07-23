0755eda00ddbc39028d62ea1c25d1f817628dbb9
winsonheng <55232476+winsonheng@users.noreply.github.com>
2023-07-24 06:58:52 +0800

    Update db settings

--- orbital_backend/settings.py
+++ orbital_backend/settings.py
@@ -203,6 +203,7 @@ if env('ENVIRONMENT') == 'DEV':
 
     }
 else:
+    DATABASES = { 'default': dj_database_url.config() }
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 
