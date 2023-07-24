051063afc784f9243fe2cae2dd22c5d21f61384f
winsonheng <55232476+winsonheng@users.noreply.github.com>
2023-07-24 14:02:54 +0800

    Fix GS Credentials issue

--- common/util/gcloud_util.py
+++ common/util/gcloud_util.py
@@ -12,7 +12,7 @@ def generate_download_signed_url_v4(blob_name):
     """
     bucket_name = 'orbital-backend'
 
-    storage_client = storage.Client.from_service_account_info(getattr(settings, 'GS_CREDENTIALS', ''))
+    storage_client = storage.Client(bucket_name, getattr(settings, 'GS_CREDENTIALS', ''))
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
 
